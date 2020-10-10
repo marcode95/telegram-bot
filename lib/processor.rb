552f027c0ec4783e22a75265fd4eb9a74297a636
Marco <marcoerhardt95@gmail.com>
2020-10-10 16:49:31 +0200

    make bot give out description and picture of pokemon

--- lib/processor.rb
+++ lib/processor.rb
@@ -7,8 +7,10 @@ class Processor
     random_index = rand(150)
     name = Reader.new.all_pkmn_data["results"][random_index]["name"]
     type = Reader.new.one_pkmn_data(random_index+1)["types"][0]["type"]["name"]
-    return name, random_index+1, type
+    description = Reader.new.description_data(random_index+1)["flavor_text_entries"][0]["flavor_text"]
+    image = "https://pokeres.bastionbot.org/images/pokemon/#{random_index+1}.png"
+    return name, random_index+1, type, description.gsub("\n", ' ').gsub("\f", ' '), image
   end
 end
 
-puts test = Processor.new.random_pkmn
+
--- lib/reader.rb
+++ lib/reader.rb
@@ -18,20 +18,17 @@ class Reader
     response = JSON.parse(response)
     response
   end
+  def description_data(pkmn_index)
+    url = "https://pokeapi.co/api/v2/pokemon-species/#{pkmn_index}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
+    response
+  end
 end
 
-#puts bla = Reader.new.one_pkmn_data(1)
-
-# puts bla = Motivate.new.one_pkmn_data(1)["types"][0]["type"]["name"]
-# puts bla = Motivate.new.one_pkmn_data(1)["types"][1]["type"]["name"]
 
 
-# h = {"count"=>1050, 
-#      "next"=>"https://pokeapi.co/api/v2/pokemon?offset=151&limit=151", 
-#      "previous"=>nil, 
-#      "results"=>
-#      [{"name"=>"bulbasaur", "url"=>"https://pokeapi.co/api/v2/pokemon/1/"}, 
-#       {"name"=>"ivysaur", "url"=>"https://pokeapi.co/api/v2/pokemon/2/"},     
-#       {"name"=>"venusaur", "url"=>"https://pokeapi.co/api/v2/pokemon/3/"}]}
+puts bla = Reader.new.description_data(1)["flavor_text_entries"][0]["flavor_text"]
+puts bla.gsub("\n", ' ').gsub("\f", ' ')
 
-# puts h["results"][rand(150)]["name"]
