caa9f125ac2da24d90d33cf925654a64528b5e80
Marco <marcoerhardt95@gmail.com>
2020-10-11 17:24:39 +0200

    format results, new line for each data

--- lib/processor.rb
+++ lib/processor.rb
@@ -2,15 +2,17 @@ require_relative 'reader.rb'
 
 class Processor 
 
-
   def find_pkmn(index=rand(150))
-    name = Reader.new.all_pkmn_data["results"][index]["name"]
-    type = Reader.new.one_pkmn_data(index+1)["types"][0]["type"]["name"]
-      if Reader.new.one_pkmn_data(index+1)["types"].length == 2
-        type_2 = Reader.new.one_pkmn_data(index+1)["types"][1]["type"]["name"]
-      end
-    description = Reader.new.description_data(index+1)["flavor_text_entries"][0]["flavor_text"]
+    
     image = "https://pokeres.bastionbot.org/images/pokemon/#{index+1}.png"
-    return name, index+1, type, type_2, description.gsub("\n", ' ').gsub("\f", ' '), image
+    name = Reader.new.all_pkmn_data["results"][index]["name"].capitalize
+    number = index+1
+    types = Reader.new.one_pkmn_data(index+1)["types"]
+    type_1 = types[0]["type"]["name"].capitalize
+    type_2 = nil
+    type_2 = ", #{types[1]["type"]["name"].capitalize}" if types.length == 2
+    description = Reader.new.description_data(index+1)["flavor_text_entries"][0]["flavor_text"].gsub("\n", ' ').gsub("\f", ' ')
+    return "#{image}\nName: #{name}\nNumber: #{number}\nType: #{type_1}#{type_2} #{nil}\nDescription: #{description}"
   end
 end
+
