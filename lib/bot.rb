9ed9073c171ad47e98a63ac059ce4e4e51ec7bc2
Marco <marcoerhardt95@gmail.com>
2020-10-12 15:51:27 +0200

    add gitignore

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,6 +2,7 @@ require 'telegram/bot'
 require_relative 'reader.rb'
 require_relative 'searcher.rb'
 require_relative 'processor.rb'
+require_relative 'config.rb'
 
 class Bot
   # rubocop:disable Metrics/AbcSize
@@ -9,7 +10,7 @@ class Bot
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Metrics/BlockLength
   def initialize
-    token = '1323012288:AAHdyNpbujJHOjofvm1dCBwOVw2-w4vLAvc'
+    token = Token.new.token
 
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
---
+++ lib/config.rb
@@ -0,0 +1,7 @@
+class Token
+  def initialize
+    @token = '1323012288:AAHdyNpbujJHOjofvm1dCBwOVw2-w4vLAvc'
+  end
+
+  attr_reader :token
+end
